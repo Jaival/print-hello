9e6651be31a690f514097089de84774c47688035
Jaival <saijajaival@gmail.com>
2024-11-25 19:23:15 +0530

    build: added new function and updated package.json

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a test package to test CI/CD for GitHub Actions and NPM.
+
+Functions that are available:
+printHello
+printName
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function printHello() {
-    return "Hello from the Package!!"
-  }
-  
-  module.exports = printHello
+export function printHello() {
+  return 'Hello from the Package!!';
+}
+
+export function printName(name) {
+  return `Hello ${name} from the Package!!`;
+}
+
+// module.exports = printHello;
+// module.exports = printName;
